853bf3070eb9bdd1f95e1a9856c37e7432f8da3b
vfedotova418-png <vfedotova418@gmail.com>
2026-03-11 08:47:31 +0300

    Step 4: Add words data and game logic

--- app/src/main/java/com/fedotova/unscramblegame/ui_model/GameViewModel.kt
+++ app/src/main/java/com/fedotova/unscramblegame/ui_model/GameViewModel.kt
@@ -1,7 +1,9 @@
 package com.fedotova.unscramblegame.ui_model
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.fedotova.unscramblegame.data.GameUiState
+import com.fedotova.unscramblegame.data.allWords
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -9,12 +11,35 @@ import kotlinx.coroutines.flow.asStateFlow
 class GameViewModel: ViewModel() {
     private val _uiState = MutableStateFlow(GameUiState())
     val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
+    private lateinit var currentWord: String
+    private var usedWords: MutableSet<String> = mutableSetOf()
 
     init {
         resetGame()
     }
 
     fun resetGame() {
+        usedWords.clear()
+        _uiState.value = GameUiState(
+            currentScrambledWord = pickRandomWordAndShuffle()
+        )
+    }
 
+    private fun shuffleCurrentWord(word: String): String {
+        val tempWord = word.toCharArray()
+        tempWord.shuffle()
+        while (String(tempWord) == word) {
+            tempWord.shuffle()
+        }
+        return String(tempWord)
+    }
+
+    private fun pickRandomWordAndShuffle(): String {
+        currentWord = allWords.random()
+        while (usedWords.contains(currentWord)) {
+            currentWord = allWords.random()
+        }
+        usedWords.add(currentWord)
+        return shuffleCurrentWord(currentWord)
     }
 }
